dbf8d70a659574368ddb83e702a8ce30b1896683
Steph Locke <stephanie.g.locke@gmail.com>
2019-06-25 19:00:40 +0100

    Correct build statements

--- 02_deployshinyproxy.sh
+++ 02_deployshinyproxy.sh
@@ -1,4 +1,4 @@
 ACR_NAME=shinyproxydemo
-az acr build --registry $ACR_NAME --image shinyproxy:v1 shinyproxy.Dockerfile
-az acr build --registry $ACR_NAME --image kube-proxy-sidecar:v1 kube-proxy-sidecar.Dockerfile
+az acr build --registry $ACR_NAME --image shinyproxy:v1 -f shinyproxy.Dockerfile .
+az acr build --registry $ACR_NAME --image kube-proxy-sidecar:v1 -f kube-proxy-sidecar.Dockerfile .
 kubectl create -f shinyproxy.yaml
