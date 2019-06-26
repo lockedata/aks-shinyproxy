94a3c1f8375742f43c03a0fc416a422f9fdb5a4e
Steph Locke <stephanie.g.locke@gmail.com>
2019-06-26 16:54:22 +0100

    Add shinyapp demo container

--- 02_deployshinyproxy.sh
+++ 02_deployshinyproxy.sh
@@ -1,4 +1,7 @@
 ACR_NAME=shinyproxydemo
+wget https://raw.githubusercontent.com/rstudio/shiny-examples/master/001-hello/app.R
+wget https://raw.githubusercontent.com/cole-brokamp/rize/master/rize/Dockerfile shinyapp.Dockerfile
+az acr build --registry $ACR_NAME --image shinyapp:v1 -f shinyapp.Dockerfile .
 az acr build --registry $ACR_NAME --image shinyproxy:v1 -f shinyproxy.Dockerfile .
 az acr build --registry $ACR_NAME --image kube-proxy-sidecar:v1 -f kube-proxy-sidecar.Dockerfile .
 kubectl create -f shinyproxy.yaml
